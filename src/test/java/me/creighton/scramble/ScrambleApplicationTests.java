4d70666f4c7044a20f697f8da23a2aa1adebd906
tomcreighton <nt.creighton@gmail.com>
2022-08-29 10:31:58 -0600

    First commit

---
+++ src/test/java/me/creighton/scramble/ScrambleApplicationTests.java
@@ -0,0 +1,13 @@
+package me.creighton.scramble;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScrambleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
